379ce621fbf8f35c4e504a073478cdfc9a1c6696
鹏鹏 <garenge@outlook.com>
2021-01-15 22:05:57 +0800

    美化界面, 添加图片尺寸设置选项

--- PPTools/ViewController.m
+++ PPTools/ViewController.m
@@ -20,12 +20,22 @@
 
 @property (weak) IBOutlet NSTextField *copiedResultL;
 
+/// 单选选中项
+@property (nonatomic, assign) BOOL isScalMode;
+@property (weak) IBOutlet NSTextField *widthValueTF;
+@property (weak) IBOutlet NSTextField *heightValueTF;
+
 @end
 
 @implementation ViewController
 
+#pragma mark action
+
+/// 点击完成
 - (IBAction)completeToGenerateCode:(NSButton *)sender {
-    
+
+    [[NSApplication sharedApplication].mainWindow makeFirstResponder:0];
+
     if (self.picSourceTF.stringValue.length > 0) {
         /**
          <img src="https://garenge-image.oss-cn-hangzhou.aliyuncs.com/mac_dev_img/%E5%88%9B%E5%BB%BAAPP-1.png" style="width:80%; height:80%; display:block;margin:0 auto">
@@ -37,7 +47,7 @@
         
         NSMutableString *resultMString = [NSMutableString string];
         //
-        [resultMString appendFormat:@"<img src=\"%@\" style=\"width:%@%%; height:%@%%; display:block;margin:0 auto\">\n", self.picSourceTF.stringValue, self.scaleValueTF.stringValue, self.scaleValueTF.stringValue];
+        [resultMString appendFormat:@"<img src=\"%@\" style=\"%@; display:block;margin:0 auto\">\n", self.picSourceTF.stringValue, [self getStyleSizeString]];
         //
         [resultMString appendFormat:@"\t<p style=\"text-align:center\">\n\t\t<span style=\"border-bottom:1px solid #ccc;color:#aaa;padding-bottom:10px;\">%@</span>\n\t</p>\n<br/>", self.picNameTF.stringValue];
         
@@ -46,14 +56,15 @@
         [self showResult:@"图片地址为空" hidden:NO animated:NO];
         [self showResult:@"" hidden:YES animated:YES];
     }
-    
-    
 }
 
+/// 点击复制
 - (IBAction)copyAction:(NSButton *)sender {
+
+    [[NSApplication sharedApplication].mainWindow makeFirstResponder:0];
+
     if (self.resultTextView.string.length > 0) {
-        
-        
+
         NSPasteboard *paste = [NSPasteboard generalPasteboard];
         [paste clearContents];
         [paste declareTypes:[NSArray arrayWithObject:NSPasteboardTypeString] owner:nil];
@@ -66,14 +77,46 @@
     }
 }
 
+- (NSString *)getStyleSizeString {
+
+    // self.scaleValueTF.stringValue
+    if (self.isScalMode) {
+        return [NSString stringWithFormat:@"width:%@%%; height:%@%%", self.scaleValueTF.stringValue, self.scaleValueTF.stringValue];
+    } else {
+        return [NSString stringWithFormat:@"width:%@px; height:%@px", self.widthValueTF.stringValue, self.heightValueTF.stringValue];
+    }
+
+}
+/// 点击单选
+- (IBAction)genderRadioAction:(NSButton *)sender {
+
+    [[NSApplication sharedApplication].mainWindow makeFirstResponder:0];
+
+    NSInteger tag = sender.tag;
+    if (tag % 2 == 1) {
+        // 第一个, 缩放比例
+        NSLog(@"我选择了1: %ld", tag);
+        self.isScalMode = YES;
+    } else {
+        // 文件尺寸
+        NSLog(@"我选择了2: %ld", tag);
+        self.isScalMode = NO;
+    }
+
+    if (self.resultTextView.string.length > 0) {
+        [self completeToGenerateCode:nil];
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.copiedResultL.stringValue = @"";
     self.copiedResultL.hidden = YES;
     // Do any additional setup after loading the view.
-}
+    self.isScalMode = YES;
 
+    [[NSApplication sharedApplication].mainWindow makeFirstResponder:0];
+}
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
